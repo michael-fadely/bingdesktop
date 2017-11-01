0883b2bed2d527e88d7339189bfe06e81dcad838
Michael Fadely <michaelwfadely@gmail.com>
2017-11-01 17:21:13 -0600

    cleanup

--- source/main.d
+++ source/main.d
@@ -79,15 +79,31 @@ int main(string[] args)
 {
 	try
 	{
-		auto result = getopt(args, "url|u",
+		auto result = getopt(args,
+				"url|u",
 				"The URL to the Bing Desktop metadata json.",
-				&jsonUrl, "output|o", "Directory to save wallpapers.", &outDir, "no-pause|n",
+				&jsonUrl,
+				
+				"output|o",
+				"Directory to save wallpapers.",
+				&outDir,
+				
+				"no-pause|n",
 				"Disable pause at the end of the program.",
-				&noPause, "force-check|f",
-				"Skip date validation and just download all wallpapers.", &forceDownload, "overwrite",
-				"Allow wallpapers to be re-downloaded and overwritten.", &allowOverwrite, "mode|m",
+				&noPause,
+				
+				"force-check|f",
+				"Skip date validation and just download all wallpapers.",
+				&forceDownload,
+				
+				"overwrite",
+				"Allow wallpapers to be re-downloaded and overwritten.",
+				&allowOverwrite,
+				
+				"mode|m",
 				`Wallpaper selection mode. Valid options are "new" (first new wallpaper) and "newest".`
-				~ `Default mode is "none"`, &wallpaperMode);
+				~ `Default mode is "none"`,
+				&wallpaperMode);
 
 		if (result.helpWanted)
 		{
@@ -100,7 +116,7 @@ int main(string[] args)
 	}
 	catch (Exception ex)
 	{
-		stdout.writeln(ex.msg);
+		stderr.writeln(ex.msg);
 		pause();
 		return -1;
 	}
@@ -117,25 +133,30 @@ int main(string[] args)
 
 	download(jsonUrl, jsonNew);
 
-	if (forceDownload || exists(jsonOld))
+	try
 	{
-		if (forceDownload || hashFile(jsonNew) != hashFile(jsonOld))
+		if (forceDownload || exists(jsonOld))
 		{
-			stdout.writeln("Downloading new images...");
-			if (checkFiles())
+			if (forceDownload || hashFile(jsonNew) != hashFile(jsonOld))
 			{
-				return -1;
+				stdout.writeln("Downloading new images...");
+				checkFiles();
+			}
+			else
+			{
+				stdout.writeln("Nothing new.");
+				remove(jsonOld);
 			}
 		}
 		else
 		{
-			stdout.writeln("Nothing new.");
-			remove(jsonOld);
+			checkFiles();
 		}
 	}
-	else if (checkFiles())
+	catch (Exception ex)
 	{
-
+		stderr.writeln(ex.msg);
+		pause();
 		return -1;
 	}
 
@@ -146,7 +167,7 @@ int main(string[] args)
 	return 0;
 }
 
-auto hashFile(in string path)
+ubyte[32] hashFile(in string path)
 {
 	auto file = File(path, "rb");
 	SHA256 digest;
@@ -174,15 +195,15 @@ Range makeValidFilename(Range)(Range filename)
 			switch (c)
 			{
 				case 0: .. case 31:
-					case '<':
-					case '>':
-					case ':':
-					case '"':
-					case '/':
-					case '\\':
-					case '|':
-					case '?':
-					case '*':
+				case '<':
+				case '>':
+				case ':':
+				case '"':
+				case '/':
+				case '\\':
+				case '|':
+				case '?':
+				case '*':
 					result.put('_');
 					break;
 
@@ -200,86 +221,71 @@ Range makeValidFilename(Range)(Range filename)
 	return result.data;
 }
 
-int checkFiles()
+void checkFiles()
 {
-	try
+	auto text = readText(jsonNew);
+	auto file = fromJSONString!(BingWallpaper[])(text);
+	string wallpaperFilename;
+
+	foreach (BingWallpaper i; file)
 	{
-		auto text = readText(jsonNew);
-		auto file = fromJSONString!(BingWallpaper[])(text);
-		string wallpaperFilename;
-
-		foreach (BingWallpaper i; file)
+		if (i.date.empty)
 		{
-			if (i.date.empty)
-			{
-				stdout.writeln("Entry missing date!");
-				continue;
-			}
-
-			// Format is YYYY-MM-DD query.jpg
-			string name = format("%s-%s-%s %s.jpg", i.date[0 .. 4], i.date[4 .. 6], i.date[6 .. 8], i.query.strip());
-
-			name = name.makeValidFilename();
-
-			string oldpath = buildNormalizedPath(outDir, format("%d.jpg", i.id));
-			string newpath = buildNormalizedPath(outDir, name);
-
-			// If the file has been copied with its original ID name,
-			// rename it to "date query.jpg"
-			// e.g: 2014-09-17 A dredge boat near the Glénan Islands.jpg
-			if (exists(oldpath))
-			{
-				stdout.writefln("Renaming %d.jpg to %s", i.id, name);
-				rename(oldpath, newpath);
-			}
-			else // Otherwise, we just try to download.
-			{
-				if (allowOverwrite || !exists(newpath))
-				{
-					stdout.writeln("Downloading: " ~ i.copyright);
-					download(i.url, newpath);
-
-					// The json is sorted newest to oldest.
-					// So if the wallpaper mode is set to newest, we set it upon download.
-					// Otherwise, we'll keep setting the string to the "first" new downloaded,
-					// then apply the wallpaper later.
-					switch (wallpaperMode)
-					{
-						default:
-							throw new Exception("WHAT EVEN");
-
-						case WallpaperMode.newest:
-							wallpaperFilename = newpath;
-							wallpaperMode = WallpaperMode.none;
-							break;
-						case WallpaperMode.first:
-							wallpaperFilename = newpath;
-							break;
-						case WallpaperMode.none:
-							break;
-					}
-				}
-				else
-				{
-					stdout.writeln("Skipping: " ~ name);
-				}
-			}
+			stdout.writeln("Entry missing date!");
+			continue;
 		}
 
-		// Use the last set "first" new wallpaper string, then set it.
-		if (!wallpaperFilename.empty)
+		// Format is YYYY-MM-DD query.jpg
+		string name = format("%s-%s-%s %s.jpg", i.date[0 .. 4], i.date[4 .. 6], i.date[6 .. 8], i.query.strip());
+
+		name = name.makeValidFilename();
+
+		string oldpath = buildNormalizedPath(outDir, format("%d.jpg", i.id));
+		string newpath = buildNormalizedPath(outDir, name);
+
+		// If the file has been copied with its original ID name,
+		// rename it to "date query.jpg"
+		// e.g: 2014-09-17 A dredge boat near the Glénan Islands.jpg
+		if (exists(oldpath))
 		{
-			setWallpaper(wallpaperFilename);
+			stdout.writefln("Renaming %d.jpg to %s", i.id, name);
+			rename(oldpath, newpath);
+		}
+		else if (allowOverwrite || !exists(newpath)) // Otherwise, we just try to download.
+		{
+			stdout.writeln("Downloading: " ~ i.copyright);
+			download(i.url, newpath);
+
+			// The json is sorted newest to oldest.
+			// So if the wallpaper mode is set to newest, we set it upon download.
+			// Otherwise, we'll keep setting the string to the "first" new downloaded,
+			// then apply the wallpaper later.
+			switch (wallpaperMode)
+			{
+				default:
+					break;
+
+				case WallpaperMode.newest:
+					wallpaperFilename = newpath;
+					wallpaperMode = WallpaperMode.none;
+					break;
+
+				case WallpaperMode.first:
+					wallpaperFilename = newpath;
+					break;
+			}
+		}
+		else
+		{
+			stdout.writeln("Skipping: " ~ name);
 		}
 	}
-	catch (Exception e)
-	{
-		stdout.writeln(e.msg);
-		pause();
-		return 1;
-	}
 
-	return 0;
+	// Use the last set "first" new wallpaper string, then set it.
+	if (!wallpaperFilename.empty)
+	{
+		setWallpaper(wallpaperFilename);
+	}
 }
 
 void setWallpaper(string filename)
@@ -313,6 +319,6 @@ void setWallpaper(string filename)
 	}
 	else
 	{
-		stdout.writeln("Sorry, wallapers can't be set your platform. They're still downloaded, though!");
+		stdout.writeln("Sorry, wallapers can't be set on your platform. They're still downloaded, though!");
 	}
 }
